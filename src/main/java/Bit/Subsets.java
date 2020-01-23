ed248e0d937320f96353ab00d26976f583562095
zhh <nenbaying@163.com>
2020-01-24 00:50:29 +0800

    subsets

--- src/main/java/Bit/Subsets.java
+++ src/main/java/Bit/Subsets.java
@@ -1,7 +1,32 @@
 package Bit;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * Created by zhh on 2020/1/24.
+ * @Author Honghan Zhu
+ * @leetcode 78
+ * @grade medium
  */
 public class Subsets {
+    List<List<Integer>> resList;
+
+    public List<List<Integer>> subsets(int[] nums) {
+        resList = new ArrayList<>();
+        resList.add(new LinkedList<>());
+        for (int i = 0; i < nums.length; i++) {
+            backTrack(nums, i, new LinkedList<>());
+        }
+        return resList;
+    }
+
+    private void backTrack(int nums[], int idx, LinkedList<Integer> tmpList) {
+        tmpList.add(nums[idx]);
+        resList.add(new LinkedList<>(tmpList));
+        for (int i = idx + 1; i < nums.length; i++) {
+            backTrack(nums, i, tmpList);
+        }
+        tmpList.removeLast();
+    }
 }

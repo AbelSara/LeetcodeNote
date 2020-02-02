f67ccba64f6bc757d67444fe43f56ab18b6f9d1e
zhh <nenbaying@163.com>
2020-02-02 11:31:17 +0800

    unique binary search tree

---
+++ src/main/java/Tree/UniqueBinarySearchTree.java
@@ -0,0 +1,20 @@
+package Tree;
+
+/**
+ * @Author Honghan Zhu
+ * @leetcode 96
+ * @grade medium
+ */
+public class UniqueBinarySearchTree {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+        return dp[n];
+    }
+}

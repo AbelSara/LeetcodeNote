a265f1e659346db183d344725471c32a3c60ce20
zhh <nenbaying@163.com>
2020-02-05 14:37:24 +0800

    fix ListNode

--- src/main/java/List/ListNode.java
+++ src/main/java/List/ListNode.java
@@ -5,8 +5,8 @@ package List;
  * @leetcode Support List Problems
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     ListNode(int x) {
         val = x;

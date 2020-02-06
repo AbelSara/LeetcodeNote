c03edee6de5ec2b33b931a7b93f85d72eb402c34
zhh <nenbaying@163.com>
2020-02-06 15:12:16 +0800

    word break

---
+++ src/main/java/BackTracking/ParlindromePartitioning.java
@@ -0,0 +1,7 @@
+package BackTracking;
+
+/**
+ * Created by zhh on 2020/2/6.
+ */
+public class ParlindromePartitioning {
+}
---
+++ src/main/java/DP/WordBreak.java
@@ -0,0 +1,27 @@
+package DP;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Author Honghan Zhu
+ * @leetcode 139
+ * @grade medium
+ */
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}

262644366f190cc859299f76b47a831b4f2ca5dd
zhh <nenbaying@163.com>
2020-02-11 16:33:53 +0800

    reverse node in k-group

---
+++ src/main/java/List/ReverseNodeInKGroup.java
@@ -0,0 +1,36 @@
+package List;
+
+import java.util.List;
+
+/**
+ * @Author Honghan Zhu
+ * @leetcode 25
+ * @grade hard
+ */
+public class ReverseNodeInKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode resNode = new ListNode(0);
+        ListNode p = resNode;
+        while (canReverse(head, k)) {
+            ListNode tailNode = head;
+            for (int i = 0; i < k; i++) {
+                ListNode tmpNode = head.next;
+                head.next = p.next;
+                p.next = head;
+                head = tmpNode;
+            }
+            p = tailNode;
+        }
+        p.next = head;
+        return resNode.next;
+    }
+
+    private boolean canReverse(ListNode head, int k) {
+        for (int i = 0; i < k; i++) {
+            if (head == null)
+                return false;
+            head = head.next;
+        }
+        return true;
+    }
+}

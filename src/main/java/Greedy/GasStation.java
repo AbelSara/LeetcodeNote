566f83be5b9e6692abd02d4c946ca8b7d4419d43
zhh <nenbaying@163.com>
2020-02-11 16:11:38 +0800

    gas station

---
+++ src/main/java/Greedy/GasStation.java
@@ -0,0 +1,27 @@
+package Greedy;
+
+/**
+ * @Author Honghan Zhu
+ * @leetcode 134
+ * @grade medium
+ */
+public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int len = gas.length;
+        int need = 0;
+        int sum = 0;
+        int start = 0;
+        for (int i = 0; i < len; i++) {
+            int delta = gas[i] - cost[i];
+            sum += delta;
+            if (sum < 0) {
+                start = i + 1;
+                need += sum;
+                sum = 0;
+            }
+        }
+        if (sum + need >= 0)
+            return start;
+        return -1;
+    }
+}

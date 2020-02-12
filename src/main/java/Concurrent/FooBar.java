11bcb607038d10194822bac8e9b91615ae7fc7a5
zhh <nenbaying@163.com>
2020-02-12 18:10:14 +0800

    print foobar alternately

---
+++ src/main/java/Concurrent/FooBar.java
@@ -0,0 +1,42 @@
+package Concurrent;
+
+
+/**
+ * Created by zhh on 2020/2/12.
+ */
+public class FooBar {
+    private int n;
+    private int state = 0;
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            synchronized (this) {
+                if (state != 0) {
+                    this.wait();
+                }
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+                state = 1;
+                this.notify();
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            synchronized (this) {
+                if (state != 1) {
+                    this.wait();
+                }
+                // printBar.run() outputs "bar". Do not change or remove this line.
+                printBar.run();
+                state = 0;
+                this.notify();
+            }
+        }
+    }
+}

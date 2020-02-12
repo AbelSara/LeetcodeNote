d1d29e20ab3cf32134fbdd656eb951b4943ed883
zhh <nenbaying@163.com>
2020-02-12 22:55:57 +0800

    merge two binary trees

---
+++ src/main/java/Tree/MergeTwoBinaryTrees.java
@@ -0,0 +1,18 @@
+package Tree;
+
+/**
+ * @Author Honghan Zhu
+ * @leetcode 617
+ * @grade easy
+ */
+public class MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+        else if (t1 == null) return t2;
+        else if (t2 == null) return t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left,t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}

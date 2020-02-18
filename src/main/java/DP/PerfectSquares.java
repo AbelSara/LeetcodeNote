f6201c6f8967e4a73d4562b90cd93c2c3ee92c54
zhh <nenbaying@163.com>
2020-02-18 13:41:41 +0800

    perfect squares

---
+++ src/main/java/DP/PerfectSquares.java
@@ -0,0 +1,22 @@
+package DP;
+
+import java.util.Arrays;
+
+/**
+ * @Author Honghan Zhu
+ * @leetcode 279
+ * @grade medium
+ */
+public class PerfectSquares {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, n + 1);
+        dp[0] = 0;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; i - j * j >= 0; j++){
+                dp[i] = Math.min(dp[i - j*j] + 1, dp[i]);
+            }
+        }
+        return dp[n];
+    }
+}

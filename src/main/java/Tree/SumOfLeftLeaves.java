86c7c4816873705447d5a800fa3d990678b49186
zhh <nenbaying@163.com>
2020-03-02 14:40:19 +0800

    sum of left leaves

---
+++ src/main/java/Tree/SumOfLeftLeaves.java
@@ -0,0 +1,24 @@
+package Tree;
+
+/**
+ * @author Honghan Zhu
+ * @leetcode 404
+ * @grade easy
+ */
+public class SumOfLeftLeaves {
+    int sum = 0;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        order(root, 0);
+        return sum;
+    }
+
+    private void order(TreeNode root, int flag){
+        if(root == null)
+            return;
+        if(root.left == null && root.right == null && flag == 1)
+            sum += root.val;
+        order(root.left, 1);
+        order(root.right, 2);
+    }
+}

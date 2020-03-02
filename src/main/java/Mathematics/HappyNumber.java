d6c35624467808acdef3584a9985c6ebfa36c0bd
zhh <nenbaying@163.com>
2020-03-02 14:30:35 +0800

    happy number

---
+++ src/main/java/Mathematics/HappyNumber.java
@@ -0,0 +1,26 @@
+package Mathematics;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Honghan Zhu
+ * @leetcode 202
+ * @grade easy
+ */
+public class HappyNumber {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while(n != 1){
+            String num = String.valueOf(n);
+            set.add(n);
+            n = 0;
+            for(int i = 0; i < num.length(); i++){
+                n += (int)Math.pow(num.charAt(i) - '0', 2);
+            }
+            if(set.contains(n))
+                break;
+        }
+        return n == 1;
+    }
+}

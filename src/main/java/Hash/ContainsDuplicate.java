bdbfaad43ad4ad2dfd4077cddde8f1fab5da7b7f
AlberSara <nenbaying@163.com>
2020-03-08 09:55:41 +0800

    contains duplicate

---
+++ src/main/java/Hash/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+package Hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Honghan Zhu
+ * @leetcode 217
+ * @grade easy
+ */
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i]))
+                return true;
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}

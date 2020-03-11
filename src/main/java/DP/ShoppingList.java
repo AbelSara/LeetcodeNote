6bb4ddd8f07cdf38e28f1d397fd627fa4ab07f85
AlberSara <nenbaying@163.com>
2020-03-11 14:43:50 +0800

    shopping list

--- src/main/java/DP/ShoppingList.java
+++ src/main/java/DP/ShoppingList.java
@@ -1,5 +1,6 @@
 package DP;
 
+
 import java.util.*;
 
 /**
@@ -7,6 +8,7 @@ import java.util.*;
  * @牛客 购物单
  */
 public class ShoppingList {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
@@ -19,50 +21,76 @@ public class ShoppingList {
                 v[i] = sc.nextInt();
                 p[i] = sc.nextInt();
                 q[i] = sc.nextInt();
+                p[i] = p[i] * v[i];
             }
-            int res = calculate(v, p, q, n, m);
+            List<List<Integer>> children = new ArrayList<>();
+            children.add(new ArrayList<>());
+            for (int i = 1; i <= m; i++) {
+                children.add(new ArrayList<>());
+                if (q[i] > 0) {
+                    children.get(q[i]).add(i);
+                }
+            }
+            int res = calculate(v, p, q, n, m, children);
             System.out.println(res);
         }
     }
 
-    private static int calculate(int[] v, int[] p, int[] q, int n, int m) {
-        int[] dp = new int[n + 1];
-        int ans = 0;
-        Map<Integer,Integer>[] map = new HashMap[m + 1];
-        for (int i = 1; i <= m; i++)
-            map[i] = new HashMap<>();
+    private static List<Pair<Integer, Integer>> cal(int idx, int[] v, int[] p, List<List<Integer>> children) {
+        List<Pair<Integer, Integer>> ret = new ArrayList<>();
+        ret.add(new Pair<>(0, 0));
+        ret.add(new Pair<>(v[idx], p[idx]));
+        for (int i = 0; i < children.get(idx).size(); i++) {
+            int key = v[idx] + v[children.get(idx).get(i)];
+            int val = p[idx] + p[children.get(idx).get(i)];
+            ret.add(new Pair<>(key, val));
+        }
+        for (int i = 0; i < children.get(idx).size(); i++) {
+            for (int j = i + 1; j < children.get(idx).size(); j++) {
+                int key = v[idx] + v[children.get(idx).get(i)] + v[children.get(idx).get(j)];
+                int val = p[idx] + p[children.get(idx).get(i)] + p[children.get(idx).get(j)];
+                ret.add(new Pair<>(key, val));
+            }
+        }
+        return ret;
+    }
 
+    private static int calculate(int[] v, int[] p, int[] q, int n, int m, List<List<Integer>> children) {
+        int[] dp = new int[n + 1];
+        int[] dp1;
+        int ans = 0;
         for (int i = 1; i <= m; i++) {
             if (q[i] == 0) {
-                for (int j = n; j >= 0 && j - v[i] >= 0; j -= 10) {
-                    if (j - v[i] == 0 || dp[j - v[i]] != 0) {
-                        dp[j] = Math.max(dp[j], dp[j - v[i]] + v[i] * p[i]);
-                        map[i].put(j, dp[j]);
+                List<Pair<Integer, Integer>> data = cal(i, v, p, children);
+                dp1 = new int[n + 1];
+                for (int k = 0; k < data.size(); k++) {
+                    for (int j = data.get(k).getKey(); j <= n; j += 10) {
+                        dp1[j] = Math.max(dp1[j], dp[j - data.get(k).getKey()] + data.get(k).getValue());
                     }
                 }
-            } else {
-                int main = q[i];
-                Integer[] arr = new Integer[map[main].size()];
-                map[main].keySet().toArray(arr);
-                Arrays.sort(arr);
-                for (int j = arr.length - 1; j >= 0; j--) {
-                    if (arr[j] + v[i] <= n && map[main].get(arr[j]) + v[i] * p[i] > dp[arr[j] + v[i]]) {
-                        dp[arr[j] + v[i]] = map[main].get(arr[j]) + v[i] * p[i];
-                        map[main].put(arr[j] + v[i], dp[arr[j] + v[i]]);
-                    }
-                }
+                dp = dp1;
             }
-            for(int j = 0; j <= n; j++){
-                if(dp[j]!=0){
-                    System.out.print(j + ":" + dp[j] +" ");
-                }
-            }
-            System.out.println();
         }
         for (int i = 0; i <= n; i++)
             ans = Math.max(ans, dp[i]);
-        System.out.println(ans);
         return ans;
+    }
+
+    static class Pair<T,E>{
+        T key;
+        E val;
+        Pair(T key, E val){
+            this.key = key;
+            this.val = val;
+        }
+
+        public T getKey() {
+            return key;
+        }
+
+        public E getValue() {
+            return val;
+        }
     }
 }
 

7dbac1df1b054059e4f6376b82f11ffd14b51cad
AlberSara <nenbaying@163.com>
2020-03-22 21:19:01 +0800

    minimum increment to make array unique

---
+++ src/main/java/Arrays/MinimumIncrementToMakeArrayUnique.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.Arrays;
+
+/**
+ * Created by Albert on 2020/3/22.
+ */
+public class MinimumIncrementToMakeArrayUnique {
+    public int minIncrementForUnique(int[] A) {
+        Arrays.sort(A);
+        int res = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] <= A[i - 1]) {
+                res += A[i - 1] - A[i] + 1;
+                A[i] = A[i - 1] + 1;
+            }
+        }
+        return res;
+    }
+}
